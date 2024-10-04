ade9653eae845ec79cb77612a522ebc3890a53ba
milosdjurica <milosdjurica13@gmail.com>
2024-10-04 13:51:10 +0200

    constants and comments

--- src/Staking.sol
+++ src/Staking.sol
@@ -5,6 +5,8 @@ import {StakingToken} from "./StakingToken.sol";
 import {AggregatorV3Interface} from "@chainlink/contracts/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract Staking {
+    uint256 public constant MINIMUM_STAKING_PERIOD = 180 days;
+
     StakingToken public immutable i_stakingToken;
     AggregatorV3Interface public immutable i_priceFeed;
 
@@ -12,4 +14,21 @@ contract Staking {
         i_stakingToken = StakingToken(stakingTokenAddr_);
         i_priceFeed = AggregatorV3Interface(priceFeedAddr_);
     }
+
+    function stakeETH() external {
+        // ! Check errors
+        // ! Calculate price
+        // ! Update storage
+        // ! Give tokens
+        // ! Emit
+    }
+    // ! Add Multiple staking
+
+    function unstakeETH() external {
+        // ! Check errors
+        // ! Update storage
+        // ! Send ETH
+        // ! Emit
+    }
+    // ! Partial unstake
 }
