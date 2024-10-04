a0ac00bd8c26ed2c081ddc426df279856ec07e9e
milosdjurica <milosdjurica13@gmail.com>
2024-10-04 12:45:05 +0200

    vscode settings, staking token and remappings

---
+++ src/StakingToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract StakingToken is ERC20, Ownable {
+    constructor() ERC20("StakingToken", "STK") Ownable(msg.sender) {}
+
+    function mint(address to_, uint256 amount_) external onlyOwner {
+        _mint(to_, amount_);
+    }
+
+    function burn(address from_, uint256 amount_) external onlyOwner {
+        _burn(from_, amount_);
+    }
+}
