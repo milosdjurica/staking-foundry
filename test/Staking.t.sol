003b1f9aaceada6f77cf0e8db7cb0a3dea5f3515
milosdjurica <milosdjurica13@gmail.com>
2024-10-07 13:45:49 +0200

    deployment script done

--- test/Staking.t.sol
+++ test/Staking.t.sol
@@ -4,18 +4,18 @@ pragma solidity ^0.8.20;
 import {Test, console2} from "forge-std/Test.sol";
 import {StakingToken} from "../src/StakingToken.sol";
 import {Staking} from "../src/Staking.sol";
+import {StakingDeployScript} from "../script/Staking.s.sol";
 
-contract StakingTokenUnitTests is Test {
+contract StakingUnitTests is Test {
     StakingToken stakingToken;
     Staking staking;
+    StakingDeployScript stakingDeployScript;
 
     address payable USER = payable(makeAddr("USER"));
 
     function setUp() public {
-        stakingToken = new StakingToken();
-        // staking = new Staking(address(stakingToken), address(0));
-        staking = new Staking(address(stakingToken));
-        stakingToken.transferOwnership(address(staking));
+        stakingDeployScript = new StakingDeployScript();
+        (stakingToken, staking) = stakingDeployScript.run();
 
         vm.deal(USER, 100 ether);
     }
